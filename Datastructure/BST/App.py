23e2b8ae0f531e246ab736248fa666fe69071dfd
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-05-20 12:39:26 -0600

    almost done with deletion method

--- Datastructure/BST/App.py
+++ Datastructure/BST/App.py
@@ -10,7 +10,31 @@ class BST:
     def __init__(self):
         self.root = None
 
-    def addHelper(self, leafToAdd: Leaf, parent: Leaf): 
+    # return the minimum value in the tree, should be on the left side 
+    def __findMinHelper(self, parent):
+        if parent.leftChild: 
+            return self.__findMinHelper(parent.leftChild)
+        return parent.value
+
+    def __findMaxHelper(self, parent):
+        if parent.rightChild: 
+            return self.__findMaxHelper(parent.rightChild)
+        return parent.value 
+         
+
+    def findMin(self): 
+        if self.root is None: 
+            print("no minimum of empty tree")
+        min = self.__findMinHelper(self.root)
+        print("MINIMUM: ",min)
+
+    def findMax(self): 
+        if self.root is None: 
+            print("no maximum of empty tree")
+        min = self.__findMaxHelper(self.root)
+        print("MAXIMUM: ",min)
+
+    def __addHelper(self, leafToAdd: Leaf, parent: Leaf): 
         pValue = parent.value 
         nValue = leafToAdd.value 
         isLessE = (nValue <= pValue)
@@ -18,14 +42,14 @@ class BST:
         # traverse down 
         if parent.leftChild and parent.rightChild:
             if isLessE:
-                self.addHelper(leafToAdd, parent.leftChild)
+                self.__addHelper(leafToAdd, parent.leftChild)
             else: 
-                self.addHelper(leafToAdd, parent.rightChild)
+                self.__addHelper(leafToAdd, parent.rightChild)
         
         # if left but no right
         elif parent.leftChild and not parent.rightChild:
             if isLessE:
-                self.addHelper(leafToAdd, parent.leftChild)
+                self.__addHelper(leafToAdd, parent.leftChild)
             else: 
                 parent.rightChild = leafToAdd
         
@@ -34,7 +58,7 @@ class BST:
             if isLessE:
                 parent.leftChild = leafToAdd
             else: 
-                self.addHelper(leafToAdd, parent.rightChild) 
+                self.__addHelper(leafToAdd, parent.rightChild) 
         
         # no child
         elif not parent.leftChild and not parent.rightChild: 
@@ -47,11 +71,46 @@ class BST:
         if self.root == None: 
             self.root = leaf 
         else: 
-            self.addHelper(leaf, self.root)
+            self.__addHelper(leaf, self.root)
 
-    # find the spot then replace with one of its child accordingly 
+    def __removeHelper(self, value, parent, prev=None): 
+        if parent is None: 
+            print(value, " does not exist in this tree.")
+            return
+
+        # look down left subtree / rightsubtree 
+        if parent.leftChild and parent.rightChild: 
+            if parent.value > value: 
+                self.__removeHelper(value, parent.leftChild, parent)
+            else: 
+                self.__removeHelper(value, parent.rightChild, parent)
+
+        elif parent.value == value:                     
+            # Case 1: No children
+            if parent.leftChild is None and parent.rightChild is None: 
+                if prev.leftChild.value == value: 
+                    prev.leftChild = None
+                else: 
+                    prev.rightChild = None
+                
+            # Case 2: 1 Child
+            elif (parent.leftChild and not parent.rightChild) or (parent.rightChild and not parent.leftChild): 
+                if parent.leftChild: 
+                    parent.value = parent.leftChild.value
+                    parent.leftChild = None 
+                elif parent.rightChild: 
+                    parent.value = parent.rightChild.value
+                    parent.rightChild = None 
+
+            # Case 3: 2 Children
+            else: 
+                pass
+
     def removeLeaf(self, value):
-        pass
+        if self.root is None: 
+            print("No tree exists.")
+        else: 
+            self.__removeHelper(value, self.root)
 
     # pass the root in intially 
     def traverse(self, leaf): 
@@ -61,29 +120,6 @@ class BST:
         self.traverse(leaf.leftChild)
         self.traverse(leaf.rightChild)        
 
-    # return the minimum value in the tree, should be on the left side 
-    def findMinHelper(self, parent):
-        if parent.leftChild: 
-            return self.findMinHelper(parent.leftChild)
-        return parent.value
-
-    def findMaxHelper(self, parent):
-        if parent.rightChild: 
-            return self.findMaxHelper(parent.rightChild)
-        return parent.value 
-         
-
-    def findMin(self): 
-        if self.root is None: 
-            print("no minimum of empty tree")
-        min = self.findMinHelper(self.root)
-        print("MINIMUM: ",min)
-
-    def findMax(self): 
-        if self.root is None: 
-            print("no maximum of empty tree")
-        min = self.findMaxHelper(self.root)
-        print("MAXIMUM: ",min)
         
 
 def main():
@@ -102,17 +138,23 @@ def main():
               19 
          4          23 
       1     11          31 
-        6
-          7
+         6
+            7
     '''
 
     seeds = [nineteen,twentyThree, thirtyOne, four, eleven, six, seven, one]
     for seed in seeds: 
         bst.addLeaf(seed)
-    bst.findMin()
-    bst.findMax()
+
+    toDelete = 1
+    bst.traverse(nineteen)
+    print("Deleting: ", toDelete)
+    bst.removeLeaf(toDelete)
     
-    #print("-----Traversing-----")
-    #bst.traverse(nineteen)
+    #bst.findMin()
+    #bst.findMax()
+    
+    print("-----Traversing-----")
+    bst.traverse(nineteen)
 
 main()
