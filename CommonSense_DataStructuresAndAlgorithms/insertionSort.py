4f51c0a153703dbd63b748b7c3e48f8af9191de7
phoubear <phoubear@pop-os.localdomain>
2022-03-24 18:47:40 -0600

    did something to insertionSort

--- CommonSense_DataStructuresAndAlgorithms/insertionSort.py
+++ CommonSense_DataStructuresAndAlgorithms/insertionSort.py
@@ -1,11 +1,16 @@
 '''
-    InsertionSort 
+    InsertionSort
+        In this algorithm we start at index 1 then shift all elements behind until we find 
+        a value that is not larger than the one we inspecting. If we do find a value that is
+        less than or equal to the index value we are inspecting than we know that the last 
+        index that was shifted has to be the index where the value that we were inspecting 
+        belongs. 
 
-
-    Runtime analysis: O(n^2)
+    Runtime Analysis: O()
 '''
 import time
 from datetime import timedelta
+import random
 
 def insertionSortTextbook(nums: [int]) -> [int]: 
     for index in range(1,len(nums)): 
@@ -26,10 +31,10 @@ def insertionSortTextbook(nums: [int]) -> [int]:
     return nums
 
 def insertionSort(nums:[int]) -> [int]:
-    for index in range(1, len(nums)): 
+    for index in range(1, len(nums)):
         valueToInsert = nums[index]
         lastShiftedIndex = index 
-        for scanPosition in range(index-1,-1,-1):  
+        for scanPosition in range(index-1,-1,-1): 
             if nums[scanPosition] > valueToInsert:  
                 nums[scanPosition+1] = nums[scanPosition]
                 lastShiftedIndex = scanPosition
@@ -74,13 +79,17 @@ def testMethods():
 
 
 def main(): 
-    testCase1 = [5,4,3,2,1]
+    testCase1 = [5,4,3,2,1] 
     testCase2 = [3,1,5]
     testCase3 = [4,2,7,1,3]
     testCase4 = [11,2,33,4,544,11,2,33,4,544,11,2,33,4,544,11,2,33,4,544,11,2,33,
                     4,544,11,2,33,4,544,11,2,33,4,544,11,2,33,4,544,11,2,33,4,544,
                         11,2,33,4,544,11,2,33,4,544,11,2,33,4,544]
-    tests = [testCase1, testCase2, testCase3, testCase4]
+    testCase5 = [] 
+    for i in range(100000): 
+        testCase5.append(i)
+    random.shuffle(testCase5)
+    tests = [testCase1, testCase2, testCase3, testCase4, testCase5]
     runRealTest = True 
     if runRealTest:
         startTests(tests)  
